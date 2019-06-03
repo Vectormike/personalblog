6b084828d38380116ed300650e803384de1b73db
Vectormike40 <victor.junior40@yahoo.com>
2019-06-03 16:21:51 +0100

    Trying to create pages but stocked.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,62 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+
 const path = require('path');
 
-exports.onCreateNode = ({ node, actions }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
     const { createNodeField } = actions
 
     if(node.internal.type === 'MarkdownRemark') {
         const slug = path.basename(node.fileAbsolutePath, '.md')
-        console.log(slug, '@@@@@@@@')
 
         createNodeField({
             node,
             name: 'slug',
             value: slug
         })
+    }    
+}  
+
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(`
+    query loadPagesQuery ($limit: Int!) {
+      allMarkdownRemark(limit: $limit) {
+        edges {
+          node {
+              fields{
+                  slug
+              }
+          }
+        }
+      }
+    }
+  `, { limit: 1000 }).then(result => {
+    if (result.errors) {
+      throw result.errors
     }
 
-}  
+    // Create blog post pages.
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        // Path for this page — required
+        path: `${edges.node.fields.slug}`,
+        component: blogPostTemplate,
+        context: {
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          context: {
+            slug: node.fields.slug
+        }
+        },
+      })
+    })
+  })
+}
