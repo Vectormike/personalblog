593bd65c0b506bb375bffb8f20e4b0868504f92d
Vectormike40 <victor.junior40@yahoo.com>
2019-06-05 01:36:55 +0100

    Post query

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,33 @@
  import React from 'react';
  import Layout from '../components/layout';
+import { graphql } from 'gatsby';
 
- const Blog = () => {
+ const BlogPost = ({data}) => {
+    const post = data.markdownRemark;
+    const { title } = post.frontmatter;
+
     return (
         <div>
             <Layout>
-                <p>This is the blog template</p>
+                <div>
+                    <h1>{title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                </div>
             </Layout>
-            
         </div>
     )
  }
 
- export default Blog;
+ export default BlogPost;
+
+ export const query = graphql`
+    query PostQuery($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
+            frontmatter {
+             title
+
+            }
+        }  
+    }
+ `
