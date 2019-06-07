1d57c227539b51af94243e5adbeb3953092af29c
Vectormike40 <victor.junior40@yahoo.com>
2019-06-07 16:29:39 +0100

    Added React share

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,16 +2,39 @@ import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 import SEO from "../components/seo"
+import BlogPostStyles from '../styles/blog-post.module.scss';
+
+import {
+   FacebookShareButton,
+   LinkedinShareButton,
+   TwitterShareButton,
+   TelegramShareButton,
+   WhatsappShareButton,
+   FacebookIcon,
+   TwitterIcon,
+   TelegramIcon,
+   WhatsappIcon
+} from 'react-share';   
 
  const BlogPost = ({data}) => {
     const post = data.markdownRemark;
     const { title } = post.frontmatter;
+    const { slug } = post.fields;
 
     return (
             <Layout>
                 <SEO title={title}/>
-                <h1>{title}</h1>
+                <h1 className={BlogPostStyles.header}>{title}</h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
+                <div>
+                  <FacebookShareButton
+                     title={title}
+                     url={`localhost:8000/${slug}`}
+                  >   
+                     <FacebookIcon size={40} round={true}/>
+                  </FacebookShareButton>
+                </div>
             </Layout>
     
     )
@@ -23,6 +46,9 @@ import SEO from "../components/seo"
  query PostQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      fields {
+         slug
+      }
       frontmatter {
        title     
     }
