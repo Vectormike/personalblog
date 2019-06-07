b0ec73cb0709cc7fcf421c4a5f5001fa6010adac
Vectormike40 <victor.junior40@yahoo.com>
2019-06-07 16:29:08 +0100

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Vectormike`,
+        short_name: `Vectormike`,
         start_url: `/`,
         background_color: `#000`,
-        theme_color: `#e5e5e5`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
