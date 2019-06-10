b2c160ca43afc97daf9c8dcbaa851d12700f674e
Vectormike40 <victor.junior40@yahoo.com>
2019-06-10 12:35:35 +0100

    Modifying footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby';
 import footerStyles from '../styles/footer.module.scss';
 
 
@@ -6,6 +7,11 @@ const Footer = ({siteAuthor}) => {
   return (
     <div className={footerStyles.footer}>
       <p>Built by {siteAuthor} © {new Date().getFullYear()}</p>
+      <p>
+        <Link>Twitter</Link>
+        <Link>Github</Link>
+        <Link>Facebook</Link>
+      </p>
     </div>
   )
 }
